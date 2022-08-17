15945e62db6087c223aa725a001db52da63d0fd8
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-08-17 14:24:27 +0000

    Agrega clase DinoSet y funciones add, to_string y contains

--- 01. DinoSet/dinoset.h
+++ 01. DinoSet/dinoset.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 const int total_dinos = 8;
 
 enum class DinoId {
@@ -12,3 +14,38 @@ enum class DinoId {
     parasaurolophus,     // 6
     pteranodon           // 7
 };
+
+class DinoSet {
+
+public:
+
+    void add(DinoId id)
+    {
+        int index = static_cast<int>(id);
+        _dino[index] = true;
+    }
+
+    std::string to_string() const
+    {
+        std::string result = "";
+        for (int i = 0; i < total_dinos; ++i) {
+            if (_dino[i]) {
+                result += "1";
+            } else {
+                result += "0";
+            }
+        }
+        return result;
+    }
+
+    bool contains(DinoId id) const
+    {
+        int index = static_cast<int>(id);
+        return _dino[index];
+    }
+
+private:
+
+    bool _dino[total_dinos] {};
+};
+
--- 01. DinoSet/main.cpp
+++ 01. DinoSet/main.cpp
@@ -3,5 +3,10 @@
 
 int main()
 {
+    DinoSet d;
+    d.add(DinoId::pteranodon);
+    d.add(DinoId::velociraptor);
+    d.add(DinoId::tyrannosaurus);
+    std::cout << d.to_string() << "\n";
     return 0;
 }
