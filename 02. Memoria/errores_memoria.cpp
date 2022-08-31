89fe272843f8f7678dde21d134bafa07dcf9b51f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-08-31 14:00:11 +0000

    Agrega código con errores detectados por valgrind

--- 02. Memoria/errores_memoria.cpp
+++ 02. Memoria/errores_memoria.cpp
@@ -2,10 +2,18 @@
 
 int main()
 {
-    int x = 5;
-    int* p = nullptr;
+    // int x = 5;
+    // int* p = nullptr;
 
-    std::cout << *p << "\n";
+    // std::cout << *p << "\n";
+
+    int a[10];
+
+    a[9] = 7;
+    // a[10] = 8;
+
+    std::cout << a[9] << "\n";
+    std::cout << a[1000] << "\n";
 
     return 0;
 }
