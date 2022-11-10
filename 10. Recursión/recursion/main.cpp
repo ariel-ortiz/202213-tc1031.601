b22cebeddf04b07bf4b45ee72e37a5192a37e2b6
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-11-10 17:47:59 +0000

    Agrega archivos iniciales para la práctica de recursión

---
+++ 10. Recursión/recursion/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "intlist.h"
+#include "recursion.h"
+
+int main()
+{
+    return 0;
+}
---
+++ 10. Recursión/recursion/recursion.cpp
@@ -0,0 +1,77 @@
+#include "intlist.h"
+#include "recursion.h"
+
+int size(const IntList& a)
+{
+    return 0;
+}
+
+IntList cons_end(int value, const IntList& a)
+{
+    return IntList {};
+}
+
+int sum(const IntList& a)
+{
+    return 0;
+}
+
+IntList duplicate(const IntList& a)
+{
+    return IntList {};
+}
+
+int last(const IntList& a)
+{
+    return 0;
+}
+
+IntList but_last(const IntList& a)
+{
+    return IntList {};
+}
+
+int maximum(const IntList& a)
+{
+    return 0;
+}
+
+IntList append(const IntList& a, const IntList& b)
+{
+    return IntList {};
+}
+
+IntList repeat(int n, int value)
+{
+    return IntList {};
+}
+
+IntList reverse(const IntList& a)
+{
+    return IntList {};
+}
+
+IntList merge(const IntList& a, const IntList& b)
+{
+    return IntList {};
+}
+
+bool is_prefix(const IntList& a, const IntList& b)
+{
+    return false;
+}
+
+IntList insert(int value, const IntList& a)
+{
+    return IntList {};
+}
+
+IntList insertion_sort(const IntList& a)
+{
+    return IntList {};
+}
+
+IntList binary(int n)
+{
+    return IntList {};
+}
