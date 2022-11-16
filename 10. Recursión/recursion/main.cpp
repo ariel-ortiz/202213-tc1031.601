4ec3df6e9a2ba75fffecfe68b1238f097a513177
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-11-16 15:10:56 +0000

    Agrega código de duplicate, last, but_last, maximum, append, repeat y merge

--- 10. Recursión/recursion/main.cpp
+++ 10. Recursión/recursion/main.cpp
@@ -4,5 +4,39 @@
 
 int main()
 {
+    IntList a {1, 2, 3, 4};
+    IntList b = rest(a);
+    IntList c = rest(b);
+    IntList d = rest(c);
+
+    std::cout << a << "\n";
+    std::cout << b << "\n";
+    std::cout << c << "\n";
+    std::cout << d << "\n";
+
+    std::cout << first(d) << "\n";
+    std::cout << first(rest(a)) << "\n";
+    std::cout << is_empty(rest(d)) << "\n";
+    std::cout << is_empty(d) << "\n";
+    std::cout << cons(42, a) << "\n";
+
+    std::cout << size(a) << "\n";
+    std::cout << cons_end(42, a) << "\n";
+    std::cout << sum(a) << "\n";
+
+    std::cout << duplicate(a) << "\n";
+
+    std::cout << last(a) << "\n";
+
+    std::cout << but_last(a) << "\n";
+    std::cout << maximum(a) << "\n";
+
+    std::cout << append(a, IntList {5, 6, 7, 8}) << "\n";
+    std::cout << append(a, IntList {}) << "\n";
+
+    std::cout << repeat(10, 3) << "\n";
+
+    std::cout << merge(IntList {4, 8, 15, 16, 23, 42}, IntList {1, 2, 5, 7, 8, 10}) << "\n";
+
     return 0;
 }
--- 10. Recursión/recursion/recursion.cpp
+++ 10. Recursión/recursion/recursion.cpp
@@ -1,49 +1,94 @@
 #include "intlist.h"
 #include "recursion.h"
 
+// Complejidad: O(N)
 int size(const IntList& a)
 {
-    return 0;
+    if (is_empty(a)) {
+        return 0;
+    } else {
+        return 1 + size(rest(a));
+    }
 }
 
+// Complejidad: O(N)
 IntList cons_end(int value, const IntList& a)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return cons(value, a);
+    } else {
+        return cons(first(a), cons_end(value, rest(a)));
+    }
 }
 
+// Complejidad: O(N)
 int sum(const IntList& a)
 {
-    return 0;
+    if (is_empty(a)) {
+        return 0;
+    } else {
+        return first(a) + sum(rest(a));
+    }
 }
 
+// Complejidad: O(N)
 IntList duplicate(const IntList& a)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return IntList {};
+    } else {
+        return cons(first(a), cons(first(a), duplicate(rest(a))));
+    }
 }
 
+// Complejidad: O(N)
 int last(const IntList& a)
 {
-    return 0;
+    if (is_empty(rest(a))) {
+        return first(a);
+    } else {
+        return last(rest(a));
+    }
 }
 
+// Complejidad: O(N)
 IntList but_last(const IntList& a)
 {
-    return IntList {};
+    if (is_empty(rest(a))) {
+        return IntList {};
+    } else {
+        return cons(first(a), but_last(rest(a)));
+    }
 }
 
+// Complejidad: O(N)
 int maximum(const IntList& a)
 {
-    return 0;
+    if (is_empty(rest(a))) {
+        return first(a);
+    } else {
+        return std::max(first(a), maximum(rest(a)));
+    }
 }
 
+// Complejidad: O(N), donde N es el tamaño de a
 IntList append(const IntList& a, const IntList& b)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return b;
+    } else {
+        return cons(first(a), append(rest(a), b));
+    }
 }
 
+// Complejidad: O(N)
 IntList repeat(int n, int value)
 {
-    return IntList {};
+    if (n == 0) {
+        return IntList {};
+    } else {
+        return cons(value, repeat(n - 1, value));
+    }
 }
 
 IntList reverse(const IntList& a)
@@ -51,9 +96,19 @@ IntList reverse(const IntList& a)
     return IntList {};
 }
 
+// Complejidad: O(N + M), donde N es el tamaño de a y M es el tamaño de b
+//              O(N) donde N es la suma de los tamaños de a y b
 IntList merge(const IntList& a, const IntList& b)
 {
-    return IntList {};
+    if (is_empty(a)) {
+        return b;
+    } else if (is_empty(b)) {
+        return a;
+    } else if (first(a) < first(b)) {
+        return cons(first(a), merge(rest(a), b));
+    } else {
+        return cons(first(b), merge(a, rest(b)));
+    }
 }
 
 bool is_prefix(const IntList& a, const IntList& b)
